c684ad2e68c41257fd5bd8a08b61e8e26a87d1ef
divaweb <lanittad@hotmail.com>
2018-06-19 10:18:37 -0600

    csv checkpoint

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,6 +7,7 @@ class AddressBook
 
   def initialize
     @entries = []
+
 end
 
 def remove_entry(name,phone_number, email)
@@ -35,6 +36,7 @@ entries.insert(index, Entry.new(name, phone_number, email))
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
     csv.each do |row|
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
