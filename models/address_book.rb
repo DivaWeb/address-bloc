4d3602ba71bd5409925d01b6e96479e90b5df790
divaweb <lanittad@hotmail.com>
2018-06-21 08:42:48 -0600

    searching assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,62 +3,72 @@ require "csv"
 
 
 class AddressBook
-  attr_reader :entries
+attr_reader :entries
 
-  def initialize
-    @entries = []
+def initialize
+  @entries = []
 
+end
+
+def remove_entry(name,phone_number, email)
+delete_entry = nil
+entries.each do |entry|
+  if name == entry.name && phone_number == entry.phone_number && email = entry.email
+    delete_entry = entry
   end
+end
+entries.delete(delete_entry)
+end
 
-  def remove_entry(name,phone_number, email)
-    delete_entry = nil
-    entries.each do |entry|
-      if name == entry.name && phone_number == entry.phone_number && email = entry.email
-        delete_entry = entry
-      end
-    end
-    entries.delete(delete_entry)
+def add_entry(name, phone_number, email)
+index = 0
+entries.each do |entry|
+
+  if name < entry.name
+    break
   end
+  index+= 1
+end
 
-  def add_entry(name, phone_number, email)
-    index = 0
-    entries.each do |entry|
+entries.insert(index, Entry.new(name, phone_number, email))
+end
 
-      if name < entry.name
-        break
-      end
-      index+= 1
-    end
+def import_from_csv(file_name)
+  csv_text = File.read(file_name)
+  csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
 
-    entries.insert(index, Entry.new(name, phone_number, email))
+  csv.each do |row|
+    row_hash = row.to_hash
+    add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
   end
+end
 
-  def import_from_csv(file_name)
-    csv_text = File.read(file_name)
-    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+def binary_search(name)
+ lower = 0
+ upper = entries.length - 1
 
-    csv.each do |row|
-      row_hash = row.to_hash
-      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-    end
+ while lower <= upper
+   mid = (lower + upper) / 2
+   mid_name = entries[mid].name
+
+   if name == mid_name
+     return entries[mid]
+   elsif name < mid_name
+     upper = mid - 1
+   elsif name > mid_name
+     lower = mid + 1
+   end
+ end
+ return nil
+end
+
+def iterative_search(name)
+  CSV.each("entries.csv") do |row|
+    if row == name
+      return row;
+    else
+      return nil
   end
-
-  def binary_search(name)
-    lower = 0
-    upper = entries.length - 1
-
-    while lower <= upper
-      mid = (lower + upper) / 2
-      mid_name = entries[mid].name
-
-      if name == mid_name
-        return entries[mid]
-      elsif name < mid_name
-        upper = mid - 1
-      elsif name > mid_name
-        lower = mid + 1
-      end
-    end
-    return nil
-  end
+end 
+end
 end
